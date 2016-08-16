0a20e6c26ac5abd0c759ac6ede07b8b71ccd10fb
Aimen Khakwani and Molly LeCompte <aimen@>
2016-08-16 16:05:53 -0700

    Add js to remove spaces

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,18 @@ $(document).ready(function() {
     event.preventDefault();
     var userWord = $("input").val();
     var splitArray = userWord.split("");
+    for(var i = 0; i < splitArray.length; i+=1) {
+      if (splitArray[i] === " "){
+        splitArray[i] = "";
+      }
+    }
     var reverseArray = splitArray.slice().reverse();
-
-
-
-    if (splitArray.toString() === reverseArray.toString()){
+    
+    if (splitArray.join("") === reverseArray.join("")){
+      $("#no-palindrome").hide();
       $("#yes-palindrome").show();
     } else if (splitArray !== reverseArray) {
+      $("#yes-palindrome").hide();
       $("#no-palindrome").show();
     }
 
